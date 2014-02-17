51e5fbe54d9de8ecc531a14e734c7bb97f55424d
Alastair Houghton <alastair@alastairs-place.net>
2014-02-17 09:58:01 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
 with open('README.rst', 'r') as f:
     long_desc = f.read().decode('utf-8')
 
 setup(name='dmgbuild',
-      version='1.0',
+      version='1.0.0',
       description='Mac OS X command line utility to build disk images',
       long_description=long_desc,
       author='Alastair Houghton',
       author_email='alastair@alastairs-place.net',
       url='http://alastairs-place.net/projects/dmgbuild',
+      license='MIT License',
       platforms='darwin',
       packages=['dmgbuild'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Desktop Environment',
+          ],
       package_data = {
         'dmgbuild': ['resources/*']
       },
