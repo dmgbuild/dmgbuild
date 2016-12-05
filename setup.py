15fb8b87345f75b618cd061fa885216882b6fa19
Alastair Houghton <alastair@alastairs-place.net>
2016-12-05 09:06:01 +0000

    Increased required version of ds_store.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'rb') as f:
 
 # We have to be able to install on Linux to build the docs, even though
 # dmgbuild presently won't work there because there's no SetFile
-requires=['ds_store >= 1.0.1',
+requires=['ds_store >= 1.1.0',
           'mac_alias >= 2.0.0',
           'six >= 1.4.1']
 
@@ -15,7 +15,7 @@ if sys.platform.startswith('darwin'):
     requires.append('pyobjc-framework-Quartz >= 3.0.4')
 
 setup(name='dmgbuild',
-      version='1.2.0',
+      version='1.2.1',
       description='Mac OS X command line utility to build disk images',
       long_description=long_desc,
       author='Alastair Houghton',
