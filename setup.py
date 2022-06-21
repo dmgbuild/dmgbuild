32c21303ec239a991d154ce7392203f382171218
Russell Keith-Magee <russell@keith-magee.com>
2022-06-21 10:31:12 +0800

    Modernize project layout.

--- setup.py
+++ setup.py
@@ -1,65 +1,48 @@
-# -*- coding: utf-8 -*-
-import sys
+#!/usr/bin/env python
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+setup()
 
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
+# # -*- coding: utf-8 -*-
+# import sys
+# from setuptools import setup
+# from setuptools.command.test import test as TestCommand
 
-with open('README.rst', 'rb') as f:
-    long_desc = f.read().decode('utf-8')
+# class PyTest(TestCommand):
+#     def finalize_options(self):
+#         TestCommand.finalize_options(self)
+#         self.test_args = []
+#         self.test_suite = True
 
-# We have to be able to install on Linux to build the docs, even though
-# dmgbuild presently won't work there because there's no SetFile
-requires=['ds_store >= 1.1.0',
-          'mac_alias >= 2.0.1']
+#     def run_tests(self):
+#         import pytest
+#         errno = pytest.main(self.test_args)
+#         sys.exit(errno)
 
-# On Python <3.4, we need biplist
-if sys.version_info < (3, 4):
-    requires.append('biplist >= 0.6')
+# with open('README.rst', 'rb') as f:
+#     long_desc = f.read().decode('utf-8')
 
-if sys.version_info.major == 2:
-    tests_require = ['pytest == 3.6.4',
-                     'py == 1.10.0',
-                     'more_itertools == 4.0.0']
-else:
-    tests_require = ['pytest']
+# # We have to be able to install on Linux to build the docs, even though
+# # dmgbuild presently won't work there because there's no SetFile
+# requires=['ds_store >= 1.1.0',
+#           'mac_alias >= 2.0.1']
 
-setup(name='dmgbuild',
-      version='1.5.2',
-      description='macOS command line utility to build disk images',
-      long_description=long_desc,
-      author='Alastair Houghton',
-      author_email='alastair@alastairs-place.net',
-      url='http://alastairs-place.net/projects/dmgbuild',
-      license='MIT License',
-      platforms='darwin',
-      packages=['dmgbuild'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'License :: OSI Approved :: MIT License',
-          'Topic :: Desktop Environment',
-          ],
-      package_data = {
-        'dmgbuild': ['resources/*']
-      },
-      entry_points = {
-        'console_scripts': [
-            'dmgbuild = dmgbuild.__main__:main'
-        ],
-      },
-      install_requires=requires,
-      tests_require=tests_require,
-      cmdclass={
-          'test': PyTest
-          },
-      provides=['dmgbuild']
-)
+
+# setup(name='dmgbuild',
+#       version='1.5.2',
+#       packages=['dmgbuild'],
+#       package_data = {
+#         'dmgbuild': ['resources/*']
+#       },
+#       entry_points = {
+#         'console_scripts': [
+#             'dmgbuild = dmgbuild.__main__:main'
+#         ],
+#       },
+#       install_requires=requires,
+#       tests_require=tests_require,
+#       cmdclass={
+#           'test': PyTest
+#           },
+#       provides=['dmgbuild']
+# )
---
+++ src/dmgbuild/__init__.py
@@ -0,0 +1,5 @@
+from .core import build_dmg
+
+__version__ = "1.5.2"
+
+__all__ = ["__version__", "build_dmg"]
